84d3509ec3616bc522797358099e618ff7c1bada
Maria <93122849+ssomarii@users.noreply.github.com>
2025-06-09 18:22:25 +0700

    feat(Menu.tsx): Добавила интерфейс пропсов и пропс для внешней обёртки компонента

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,9 +1,13 @@
 import { MenuItem } from '../MenuItem';
 import styles from './menu.module.scss';
 
-export const Menu = () => {
+interface IProps {
+    stylesWrap?: string;
+}
+
+export const Menu = ({stylesWrap = ''}: IProps) => {
     return(
-        <div className={styles.menu}>
+        <div className={`${styles.menu} ${stylesWrap}`}>
             <MenuItem title='1 пункт меню' />
             <MenuItem title='2 пункт меню' />
             <MenuItem title='3 пункт меню' />
