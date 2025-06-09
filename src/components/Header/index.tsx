8df3bc1021f0a7d741664aabfd6d2514a51a6839
Maria <93122849+ssomarii@users.noreply.github.com>
2025-06-09 18:21:36 +0700

    feat(Header): Изменения в компоненте Header
    - Изменения стилей
    - Добавлена заглушка аватарки

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,15 @@
 import styles from './header.module.scss';
+import UserAvatar from '../../assets/images/userImage.jpg';
 
 export const Header = () => {
     return(
-        <div className={styles.header}>Header</div>
+        <div className={styles.header}>
+            <span>Header</span>
+            <div>
+                <div className={styles.header__avatarWrap}>
+                    <img className={styles.header__avatar} src={UserAvatar} />
+                </div>
+            </div>
+        </div>
     )
 }
