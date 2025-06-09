cee368b25e87807cc481becbbecd69fbd76a3d36
Maria <93122849+ssomarii@users.noreply.github.com>
2025-06-09 18:24:40 +0700

    feat(MenuPage): Изменения в компоненте страницы MenuPage
    - Обновлён вывод составляющих компонентов
    - Изменены стили

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,15 +1,20 @@
+import { Card } from '../../components/Card';
 import { Footer } from '../../components/Footer';
 import { Header } from '../../components/Header';
 import { Menu } from '../../components/Menu';
+import { CARDS_MOCK } from '../../constants/mocks';
 import styles from './mainPage.module.scss';
 
 export const MainPage = () => {
     return(
         <div className={styles.mainPage}>
             <div className={styles.mainPage__container}>
-                <Menu />
-                <div className={styles.mainPage__content}>
+                <Menu stylesWrap={styles.mainPage__menuWrapper} />
+                <div className={styles.mainPage__fullContent}>
                     <Header />
+                    <div className={styles.mainPage__content}>
+                        {CARDS_MOCK.map(item => <Card key={item.id} title={item.title} />)}
+                    </div>
                 </div>
             </div>
             <Footer />
