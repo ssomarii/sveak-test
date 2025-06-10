50151ce5e842308f21f9a515554c50b74cd7685d
Maria <93122849+ssomarii@users.noreply.github.com>
2025-06-11 01:34:44 +0700

    feat(Mocks): Обновила мок карточек

--- src/constants/mocks.ts
+++ src/constants/mocks.ts
@@ -1,53 +1,89 @@
-import type { TMenuItem, TCardItem } from "../types"
+import type { TMenuItem, TCardItem } from "../types";
+import Cat1 from '../assets/images/1.jpg';
+import Cat2 from '../assets/images/2.jpg';
+import Cat3 from '../assets/images/3.jpg';
+import Cat4 from '../assets/images/4.jpg';
+import Cat5 from '../assets/images/5.jpg';
+import Cat6 from '../assets/images/6.jpg';
+import Cat7 from '../assets/images/7.jpg';
+import Cat8 from '../assets/images/8.jpg';
+import Cat9 from '../assets/images/9.jpg';
+import Cat10 from '../assets/images/10.jpg';
+import Cat11 from '../assets/images/11.jpg';
+import Cat12 from '../assets/images/12.jpg';
+import Cat13 from '../assets/images/13.jpg';
+import Cat14 from '../assets/images/14.jpg';
 
 export const CARDS_MOCK: TCardItem[] = [
     {
         id: 1,
         title: 'Card 1',
+        image: Cat1,
     },
     {
         id: 2,
         title: 'Card 2',
+        image: Cat2,
     },
     {
         id: 3,
         title: 'Card 3',
+        image: Cat3,
     },
     {
         id: 4,
         title: 'Card 4',
+        image: Cat4,
     },
         {
         id: 5,
         title: 'Card 5',
+        image: Cat5,
     },
     {
         id: 6,
         title: 'Card 6',
+        image: Cat6,
     },
     {
         id: 7,
         title: 'Card 7',
+        image: Cat7,
     },
     {
         id: 8,
         title: 'Card 8',
+        image: Cat8,
     },
             {
         id: 9,
         title: 'Card 9',
+        image: Cat9,
     },
     {
         id: 10,
         title: 'Card 10',
+        image: Cat10,
     },
     {
         id: 11,
         title: 'Card 11',
+        image: Cat11,
     },
     {
         id: 12,
         title: 'Card 12',
+        image: Cat12,
+    },
+        {
+        id: 13,
+        title: 'Card 13',
+        image: Cat13,
+    },
+    {
+        id: 14,
+        title: 'Card 14',
+        image: Cat14,
     },
 ]
 
