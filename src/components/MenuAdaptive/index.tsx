2b83e859d0bff9e8ba9a4cec8945e09394e57ea1
Maria <93122849+ssomarii@users.noreply.github.com>
2025-06-11 01:34:11 +0700

    feat(MenuAdaptive): Добавила скрытие скролла при открытии меню

--- src/components/MenuAdaptive/index.tsx
+++ src/components/MenuAdaptive/index.tsx
@@ -2,6 +2,7 @@ import clsx from "clsx";
 import type { TMenuItem } from "../../types";
 import { MenuItem } from "../MenuItem";
 import styles from './menuAdaptive.module.scss';
+import { useEffect } from "react";
 
 interface IProps {
     stylesWrap?: string;
@@ -10,6 +11,16 @@ interface IProps {
 }
 
 export const MenuAdaptive = ({stylesWrap = '', listItems = [], isActive = false}: IProps) => {
+    useEffect(() => {
+        if (isActive) {
+            document.body.style.overflow = 'hidden';
+            document.body.style.paddingRight = `${window.innerWidth - document.documentElement.clientWidth}px`;
+        } else {
+            document.body.style.overflow = '';
+            document.body.style.paddingRight = '';
+        }
+    }, [isActive])
+
     return(
         <div className={clsx(styles.menuAdaptive, isActive && styles.menuAdaptive_active, stylesWrap)}>
             <div className={clsx(styles.menuAdaptive__menu, isActive && styles.menuAdaptive__menu_active)}>
