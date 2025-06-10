7eac9cdc9cbcdf87528de711f94052ca5472bfc3
Maria <93122849+ssomarii@users.noreply.github.com>
2025-06-10 23:53:39 +0700

    feat(MainPage): Добавила адаптивное меню и обработку стейта и клика по бургеру

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -2,16 +2,25 @@ import { Card } from '../../components/Card';
 import { Footer } from '../../components/Footer';
 import { Header } from '../../components/Header';
 import { Menu } from '../../components/Menu';
+import { MenuAdaptive } from '../../components/MenuAdaptive';
 import { CARDS_MOCK, MENU_ITEMS_MOCK } from '../../constants/mocks';
 import styles from './mainPage.module.scss';
+import { useState } from 'react';
 
 export const MainPage = () => {
+    const [isActiveMenu, setIsActiveMenu] = useState(false);
+
+    function _onClickButtonMenu() {
+        setIsActiveMenu(!isActiveMenu);
+    }
+    
     return(
         <div className={styles.mainPage}>
             <div className={styles.mainPage__container}>
+                <MenuAdaptive isActive={isActiveMenu} stylesWrap={styles.mainPage__menuAdaptiveWrapper} listItems={MENU_ITEMS_MOCK} />
                 <Menu listItems={MENU_ITEMS_MOCK} stylesWrap={styles.mainPage__menuWrapper} />
                 <div className={styles.mainPage__fullContent}>
-                    <Header />
+                    <Header isActiveMenu={isActiveMenu} onClickButtonMenu={_onClickButtonMenu} />
                     <div className={styles.mainPage__content}>
                         {CARDS_MOCK.map(item => <Card key={item.id} title={item.title} />)}
                     </div>
