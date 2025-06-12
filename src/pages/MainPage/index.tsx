dad0e5d450b4cd5b0b8053d9fe3c6e20b0bd845c
Maria <93122849+ssomarii@users.noreply.github.com>
2025-06-12 23:25:39 +0700

    feat(hide cards feature): Добавила фичу скрытия карточек незаконченного ряда

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -5,7 +5,7 @@ import { Menu } from '../../components/Menu';
 import { MenuAdaptive } from '../../components/MenuAdaptive';
 import { CARDS_MOCK, MENU_ITEMS_MOCK } from '../../constants/mocks';
 import styles from './mainPage.module.scss';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const MainPage = () => {
     const [isActiveMenu, setIsActiveMenu] = useState(false);
@@ -14,6 +14,34 @@ export const MainPage = () => {
         setIsActiveMenu(!isActiveMenu);
     }
     
+    
+    function hideCards() {
+        const gridContainer = document.querySelector('[data-grid-container]');
+        const gridStyles = getComputedStyle(gridContainer!);
+        const columnsCount = gridStyles.gridTemplateColumns.split(' ').length;
+        
+        let redundantCardsCount = CARDS_MOCK.length % columnsCount;
+        let allItems = Array.from(document.querySelectorAll('[data-grid-card]'));     
+        let redundantCards = allItems.slice(CARDS_MOCK.length - redundantCardsCount)
+
+        allItems.forEach((el: any) => {
+            el.style.removeProperty('--colorProp');
+        })
+
+        redundantCards.forEach((el: any) => {
+            el.style.setProperty('--colorProp', 'blue');
+            document.documentElement.style.setProperty('--columnCountProp', String(columnsCount));
+        })
+    }
+
+    window.addEventListener('resize', () => {
+        hideCards();
+    })
+
+    useEffect(() => {
+        hideCards();
+    }, []);
+    
     return(
         <div className={styles.mainPage}>
             <div className={styles.mainPage__container}>
@@ -21,8 +49,8 @@ export const MainPage = () => {
                 <Menu listItems={MENU_ITEMS_MOCK} stylesWrap={styles.mainPage__menuWrapper} />
                 <div className={styles.mainPage__fullContent}>
                     <Header isActiveMenu={isActiveMenu} onClickButtonMenu={_onClickButtonMenu} />
-                    <div className={styles.mainPage__content}>
-                        {CARDS_MOCK.map(item => <Card key={item.id} title={item.title} image={item.image} />)}
+                    <div data-grid-container className={styles.mainPage__content}>
+                        {CARDS_MOCK.map(item => <Card stylesWrap={styles.mainPage__contentItem} key={item.id} title={item.title} image={item.image} />)}
                     </div>
                 </div>
             </div>
