b5a0c8ed4c7380df831d4d8d369d30ec2b84c9de
Maria <93122849+ssomarii@users.noreply.github.com>
2025-06-12 21:46:01 +0700

    feat(Card): Добавила внешнюю обёртку для стилей и дата-атрибут

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,13 +1,15 @@
+import clsx from 'clsx';
 import styles from './card.module.scss';
 
 interface IProps {
     title: string;
     image: string;
+    stylesWrap?: string;
 }
 
-export const Card = ({title = 'default', image = ''}: IProps) => {
+export const Card = ({title = 'default', image = '', stylesWrap = ''}: IProps) => {
     return(
-        <div className={styles.card}>
+        <div data-grid-card className={clsx(styles.card, stylesWrap)}>
             <img src={image} className={styles.card__image} />
             <div className={styles.card__contentField}>
                 <span className={styles.card__contentText}>{title}</span>
