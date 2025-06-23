b976b9ad2156670bc616aef8ae34b8b545866611
mariya.s <mariya.s@pyrobyte.ru>
2025-06-23 17:22:18 +0700

    feat(Menu): Объединила адаптивное меню в одно - универсальное

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,16 +1,31 @@
-import type {TMenuItem} from '../../types';
-import {MenuItem} from '../MenuItem';
+import clsx from "clsx";
+import type { TMenuItem } from "../../types";
+import { MenuItem } from "../MenuItem";
 import styles from './menu.module.scss';
+import { useEffect } from "react";
 
 interface IProps {
     stylesWrap?: string;
     listItems: TMenuItem[];
+    isActive: boolean;
 }
 
-export const Menu = ({stylesWrap = '', listItems = []}: IProps) => {
-    return (
-        <nav className={`${styles.menu} ${stylesWrap}`}>
-            {listItems.map(item => <MenuItem key={item.id} link={item.link} title={item.title}/>)}
+export const Menu = ({stylesWrap = '', listItems = [], isActive = false}: IProps) => {
+    useEffect(() => {
+        if (isActive) {
+            document.body.style.overflow = 'hidden';
+            document.body.style.paddingRight = `${window.innerWidth - document.documentElement.clientWidth}px`;
+        } else {
+            document.body.style.overflow = '';
+            document.body.style.paddingRight = '';
+        }
+    }, [isActive])
+
+    return(
+        <nav className={clsx(styles.menu, isActive && styles.menu_active, stylesWrap)}>
+            <div className={clsx(styles.menu__menu, isActive && styles.menu__menu_active)}>
+                {listItems.map(item => <MenuItem key={item.id} link={item.link} title={item.title} />)}
+            </div>
         </nav>
     )
 }
--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,13 +1,12 @@
-import { Card } from '../../components/Card';
-import { Footer } from '../../components/Footer';
-import { Header } from '../../components/Header';
-import { Menu } from '../../components/Menu';
-import { MenuAdaptive } from '../../components/MenuAdaptive';
-import { CARDS_MOCK, MENU_ITEMS_MOCK } from '../../constants/mocks';
-import { hideRedundantCards } from '../../helpers/hideRedundantCards';
+import {Card} from '../../components/Card';
+import {Footer} from '../../components/Footer';
+import {Header} from '../../components/Header';
+import {CARDS_MOCK, MENU_ITEMS_MOCK} from '../../constants/mocks';
+import {hideRedundantCards} from '../../helpers/hideRedundantCards';
 import styles from './mainPage.module.scss';
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 import {BurgerButton} from "../../components/BurgerButon";
+import {Menu} from "../../components/Menu";
 
 export const MainPage = () => {
     const [isActiveMenu, setIsActiveMenu] = useState(false);
@@ -25,20 +24,22 @@ export const MainPage = () => {
         };
     }, []);
 
-    return(
+    return (
         <div className={styles.mainPage}>
             <div className={styles.mainPage__container}>
-                <BurgerButton isActive={isActiveMenu} onClickButton={_onClickButtonMenu} />
-                <MenuAdaptive isActive={isActiveMenu} stylesWrap={styles.mainPage__menuAdaptiveWrapper} listItems={MENU_ITEMS_MOCK} />
-                <Menu listItems={MENU_ITEMS_MOCK} stylesWrap={styles.mainPage__menuWrapper} />
+                <BurgerButton isActive={isActiveMenu} onClickButton={_onClickButtonMenu}/>
+                <Menu isActive={isActiveMenu} stylesWrap={styles.mainPage__menuAdaptiveWrapper}
+                      listItems={MENU_ITEMS_MOCK}/>
                 <div className={styles.mainPage__fullContent}>
-                    <Header />
+                    <Header/>
                     <main data-grid-container className={styles.mainPage__content}>
-                        {CARDS_MOCK.map(item => <Card imageSmall={item.imageSmall} stylesWrap={styles.mainPage__contentItem} key={item.id} title={item.title} image={item.image} />)}
+                        {CARDS_MOCK.map(item => <Card imageSmall={item.imageSmall}
+                                                      stylesWrap={styles.mainPage__contentItem} key={item.id}
+                                                      title={item.title} image={item.image}/>)}
                     </main>
                 </div>
             </div>
-            <Footer />
+            <Footer/>
         </div>
     )
 }
